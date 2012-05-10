28424c6b0da2f50f9bda951f89b6bbf6464bc53c
Aaron Gutierrez <groffhibbitz@yahoo.com>
2012-05-09 18:58:25 -0700

    update to filter to make it way simpler

--- filter.py
+++ filter.py
@@ -1,5 +1,6 @@
 import sys
-
+from sets import Set
+import copy
 import os
 import os.path
 import logging
@@ -17,12 +18,20 @@ fh.setFormatter(formatter)
 logger.addHandler(fh)
 
 
-def test(ints):
-    #logger.debug('going to test with %s' %ints)
+def luhn_test(ints_tuple):
+    #logger.debug('going to test with %s' %ints_tuple)
 
-    double = True
+    # double every other digit starting at the end..so if number is odd, we do not double first number
+    double = False
+    if len(ints_tuple)%2 == 0:
+        double = True
+    else: 
+        double = False
+
     total = 0
-    for x in ints:
+    
+    for idx, tup in enumerate(ints_tuple):
+        x = tup[1]
         if double:
             temp = x * 2
             if temp > 9:
@@ -35,58 +44,61 @@ def test(ints):
             total += x
             double = True
 
-        #logger.debug("s: %s total:%s" %(total, x))
+
+        if idx == 13:
+            if total % 10 == 0:
+                pass
 
     if total % 10 == 0:
+        #logger.debug('returning true')
         return True
     else: 
+        #logger.debug('total is %s returning false' %total)
         return False
 
 
-logger.debug('going to do stuff')
+#logger.debug('going to do stuff')
+
+def do_filter(line):
+    out = list(line)
+
+    to_test = get_lists_to_test(line)
+    
+    for test_list in to_test:
+        if luhn_test(test_list):
+            for x in test_list:
+                out[x[0]] = 'X'
+
+    return ''.join(out)
+
+def get_lists_to_test(line):
+    to_test = []
+
+    dbuff = []
+
+    for i, x in enumerate(line):
+        if x.isdigit():
+            dbuff.append((i, int(x)))
+        elif x in [' ', '-']:
+            continue
+        else:
+            if len(dbuff) > 13:
+                for i in range(13,16):
+                    num_sublists = len(dbuff) - i 
+
+                    sublists = [dbuff[j:j+i+1] for j in range (0, num_sublists)]
+                          
+                    #logger.debug('got sublists: %s' %sublists)
+
+                    to_test.extend(sublists)
+
+            dbuff = []
+    
+    #logger.debug('to_test is %s' %to_test)
+
+    return to_test
 
 for line in sys.stdin:
-    output = ""
-
-    buf = []
-    current_pass = False
-
-    index = 0
-    crossout = []
-    for x in line:
-        try:
-            num = int(x)
-
-            buf.append(num)
-
-            current_pass = test(buf)
-                
-            #logger.debug("got test result of %s" %current_pass)
-
-            if len(buf) == 14 and current_pass == True:
-                # TODO use a dict not a list for crossout
-                crossout.extend([index - i for i in range(0,14)])
-                logger.debug('found a match: %s' %buf)
-                #logger.debug('crossout is: %s' %crossout)
-                
-
-        except ValueError:
-            # clear buffer only if not a space or dash
-            if x == ' ' or x == '-':
-                pass
-            else:
-                buf = []
-        finally:
-            if len(buf) > 16:
-                buf = buf[1:]
-            index += 1
-
-        #output += x
-
-    for idx, val in enumerate(line):
-        if idx in crossout:
-            output += 'X'
-        else:
-            output += val
+    output = do_filter(line)
 
     print output,
---
+++ mytest.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Passes test data through mask.sh and verifies the output.
+echo 56613959932537 | python filter.py
+
+
